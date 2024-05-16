fbd836daf9521deaff40f0d34e83e59494b1af88
Kelly Phan <kelly@Kellys-MacBook-Pro-10.local>
2024-05-16 15:28:24 -0700

    deploy on google

--- app.py
+++ app.py
@@ -65,6 +65,5 @@ def reset():
     session.pop('conversation_history', None)
     return jsonify({'message': 'Conversation history has been reset.'})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
